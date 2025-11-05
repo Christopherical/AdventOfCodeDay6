98b22052cfa20eb10c3eb4070621780c4a29fdf5
Christopherical <chris_dixon1989@hotmail.com>
2025-11-05 11:08:18 +0000

    Second Attempt

--- partTwo.cpp
+++ partTwo.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <chrono>
+#include <ctime>
+
+
 
 /*
 https://observablehq.com/@mbostock/advent-of-code-2022-day-6
@@ -21,6 +25,7 @@ test10 = `zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw` // first marker after character 26
 How many characters need to be processed before the first start - of - message marker is detected ?
 */
 
+// 0.001098s / 0.001005s 
 int messageStartIndex2(std::string code){
     std::set<char> distinctChars;
     const int MAX_CODE_LENGTH = code.size();
@@ -46,10 +51,43 @@ int messageStartIndex2(std::string code){
     }
     return 9999;
 }
+// 0.000997s
+int messageStartIndex3(std::string code){
+    const int blocksOfFourteen = code.size() - 14;
+
+    for(int i = 0; i <= blocksOfFourteen; i++){
+        std::set<char> charCheck;
+        for(int j = 0; j < 14; j++){
+            charCheck.emplace(code[(j+i)]);
+        }
+        if(charCheck.size() == 14){
+            std::cout << "The Index: " << std::to_string(i+14) << std::endl;
+            return (i + 14);
+        }
+    }
+    return 0;
+}
 
 int main(){
-    std::string message = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
-    int firstMarker = messageStartIndex2(message);
+    std::string test6 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"; // 19
+    std::string test7 = "bvwbjplbgvbhsrlpgdmjqwftvncz"; // 23
+    std::string test8 = "nppdvjthqldpwncqszvftbrmjlhg"; // 23
+    std::string test9 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"; // 29
+    std::string test10 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"; // 26
+    std::string tests[5] = {test6, test7, test8, test9, test10};
+
+    auto start = std::chrono::system_clock::now();
+    for(auto test : tests){
+        int firstMarker = messageStartIndex3(test);
+    }
+    auto end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    std::cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds.count() << "s"
+              << std::endl;
 
     return 0;
 }
